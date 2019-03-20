b51580f719f3b8de361d1bf474e2ff3ddb90f37b
Francisca <frastengo@gmail.com>
2019-03-20 16:35:54 -0700

    completed app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,51 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      data: ["ice cream", "peanut butter", "cookie dough", "cheese", "chocolate"],
+      inputText: ""
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+
+
+  }
+
+  handleInputChange(event){
+    this.setState ({
+      inputText: event.target.value
+    })
+  }
+
+
   render() {
+    let filtered = this.state.data.filter((element, index)=> {
+      return element.startsWith(this.state.inputText)
+    })
+    
+    console.log(filtered)
+
+    let list = filtered.map((element,index)=> {
+      return <h2 key={index}>{element}</h2>
+    })
+
+    // let inputText = event.target.value;
+    // let list = inputText.filter(function(element, index) {
+    //   if (inputText.startsWith(element) === 'true') {
+    //     return <h2>{inputText}</h2>
+    //   }
+    //   }
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          onChange={this.handleInputChange} 
+          />
+        <span>{list}</span>
       </div>
     );
   }
